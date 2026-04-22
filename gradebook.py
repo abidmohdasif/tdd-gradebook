c7230fb6109f7974d68946a33f29223089fd68d7
abidmohdasif <abidmohdasif@gmail.com>
2026-04-22 08:23:18 -0400

    implementation for average score

--- gradebook.py
+++ gradebook.py
@@ -20,6 +20,8 @@ def is_passing(score):
     return score >= 60
 
 def average(scores):
+    if len(scores) == 0:
+        raise ValueError("Scores list cannot be Empty")
     return round(sum(scores) / len(scores), 2)
 
 def curved_score(score, bonus):
