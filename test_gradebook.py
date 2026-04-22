a53649426fd58b0299be8444136b8f826271c837
abidmohdasif <abidmohdasif@gmail.com>
2026-04-22 08:37:18 -0400

    added basic test for curved score

--- test_gradebook.py
+++ test_gradebook.py
@@ -1,5 +1,5 @@
 import pytest
-from gradebook import letter_grade, is_passing, average
+from gradebook import letter_grade, is_passing, average, curved_score
 
 def test_letter_grade_A():
     assert letter_grade(95) == "A"
@@ -39,3 +39,6 @@ def test_averge_not_a_list():
 def test_average_bad_items():
     with pytest.raises(TypeError):
         average([80,"90",70])
+
+def test_curved_score_basic():
+    assert curved_score(80, 5) = 85
