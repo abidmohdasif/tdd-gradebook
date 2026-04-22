ee6413ad74d2185091329d8d1f321cd23ba56cbb
abidmohdasif <abidmohdasif@gmail.com>
2026-04-22 08:27:35 -0400

    added type error is instance

--- gradebook.py
+++ gradebook.py
@@ -20,6 +20,8 @@ def is_passing(score):
     return score >= 60
 
 def average(scores):
+    if not isinstance(scores, list):
+        raise TypeError("scores must be a list")
     if len(scores) == 0:
         raise ValueError("Scores list cannot be Empty")
     return round(sum(scores) / len(scores), 2)
