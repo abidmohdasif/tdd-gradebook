4b23d83e3c650c8706424b72d9036ca86ef31081
abidmohdasif <abidmohdasif@gmail.com>
2026-04-22 07:50:26 -0400

    letter_grade finished

--- gradebook.py
+++ gradebook.py
@@ -1,4 +1,7 @@
 def letter_grade(score):
+    if not isinstance(score, (int, float)):
+        raise TypeError("Score must be a intger/float")
+
     if score >= 90:
         return "A"
     elif score >= 80:
