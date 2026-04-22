f1ea5f1d622e7212d39e80a420eebaf3b853d116
abidmohdasif <abidmohdasif@gmail.com>
2026-04-22 08:45:14 -0400

    implementation for bonus negative

--- gradebook.py
+++ gradebook.py
@@ -29,4 +29,6 @@ def average(scores):
     return round(sum(scores) / len(scores), 2)
 
 def curved_score(score, bonus):
+    if bonus < 0:
+        raise ValueError("Bonus cannot be negative")
     return min(score + bonus, 100)
