994f0a217e4428ac44e5e0ee64bd5cfc87ddc9f1
abidmohdasif <abidmohdasif@gmail.com>
2026-04-22 08:25:42 -0400

    added a not a list test for avergae

--- test_gradebook.py
+++ test_gradebook.py
@@ -31,3 +31,7 @@ def test_average_works():
 def test_average_empty():
     with pytest.raises(ValueError):
         average([])
+
+def test_averge_not_a_list():
+    with pytest.raises(TypeError):
+        average("HIIIIII")
